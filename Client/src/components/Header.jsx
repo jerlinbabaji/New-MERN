b6acbe72341d48dba2cee478841f8c2e34b53a53
Jerlin Abraham <abrahamjerlin@gmail.com>
2024-06-14 19:46:25 +0530

    create the header section

---
+++ Client/src/components/Header.jsx
@@ -0,0 +1,63 @@
+import React from 'react';
+import { Button, Navbar, TextInput } from 'flowbite-react';
+import { Link ,useLocation} from 'react-router-dom';//use loacation is used to highlight when we place the mouse pointer to about,home,projects,this is activated below by the line <Navbar.Link active={path === "/Home"}>
+import { AiOutlineSearch } from 'react-icons/ai';
+import { FaMoon } from 'react-icons/fa';
+
+function Header() {
+    const path=useLocation().pathname;
+  return (
+    <Navbar className='border-b-2 flex justify-between items-center p-4'>
+      <Link
+        to='/'
+        className='self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white'
+      >
+        <span className='px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white'>
+          Jerlin's
+        </span>
+        Blog           
+      </Link>
+      <form className='flex items-center'>
+        <TextInput 
+          type='text'
+          placeholder='Search...'
+          className='hidden lg:block'
+        />
+        <Button className='w-12 h-10 ml-2' color='gray' pill>
+          <AiOutlineSearch />
+        </Button>
+      </form>
+      <div className='flex gap-2 md:order-2'>
+        <Button className='w-12 h-10 hidden sm:inline' color='gray' pill>
+          <FaMoon />
+        </Button>
+        <Link to='/sign-in'>
+          <Button gradientDuoTone='purpleToBlue'>
+            Sign In
+          </Button>
+        </Link>
+        <Navbar.Toggle/>
+      </div>
+      <Navbar.Collapse>
+        {/* //nested anchor tags are not possible,it will throw error,that is link within a link is not allowed,so you should add "as={'div}" */}
+        <Navbar.Link active={path === "/Home"} as={'div'}>
+          <Link to='/Home'>
+            Home
+          </Link>
+        </Navbar.Link>
+        <Navbar.Link active={path === "/about"} as={'div'}>
+          <Link to='/about'>
+            About
+          </Link>
+        </Navbar.Link>
+        <Navbar.Link active={path === "/projects"} as={'div'}>
+          <Link to='/projects'>
+            Projects
+          </Link>
+        </Navbar.Link>
+      </Navbar.Collapse>
+    </Navbar>
+  );
+}
+
+export default Header;
