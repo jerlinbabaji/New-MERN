d471c11b0ce714334fb5bd75be8d7c53c9ed9c8a
Jerlin Abraham <abrahamjerlin@gmail.com>
2024-06-18 14:25:20 +0530

    update haeder component with user data

--- Client/src/components/Header.jsx
+++ Client/src/components/Header.jsx
@@ -1,11 +1,29 @@
 import React from 'react';
-import { Button, Navbar, TextInput } from 'flowbite-react';
+import { Button, Navbar, TextInput,Dropdown,Avatar } from 'flowbite-react';
 import { Link ,useLocation} from 'react-router-dom';//use loacation is used to highlight when we place the mouse pointer to about,home,projects,this is activated below by the line <Navbar.Link active={path === "/Home"}>
 import { AiOutlineSearch } from 'react-icons/ai';
 import { FaMoon } from 'react-icons/fa';
+import { useSelector } from 'react-redux';
 
 function Header() {
     const path=useLocation().pathname;
+    const {currentUser}=useSelector(state=>state.user)
+
+    const handleSignout = async () => {
+      try {
+        const res = await fetch('/api/user/signout', {
+          method: 'POST',
+        });
+        const data = await res.json();
+        if (!res.ok) {
+          console.log(data.message);
+        } else {
+          dispatch(signoutSuccess());
+        }
+      } catch (error) {
+        console.log(error.message);
+      }
+    };
   return (
     <Navbar className='border-b-2 flex justify-between items-center p-4'>
       <Link
@@ -31,11 +49,33 @@ function Header() {
         <Button className='w-12 h-10 hidden sm:inline' color='gray' pill>
           <FaMoon />
         </Button>
-        <Link to='/sign-in'>
-          <Button gradientDuoTone='purpleToBlue' outline>
-            Sign In
-          </Button>
-        </Link>
+        {currentUser ? (
+          <Dropdown
+            arrowIcon={false}
+            inline
+            label={
+              <Avatar alt='user' img={currentUser.profilePicture} rounded />
+            }
+          >
+            <Dropdown.Header>
+              <span className='block text-sm'>@{currentUser.username}</span>
+              <span className='block text-sm font-medium truncate'>
+                {currentUser.email}
+              </span>
+            </Dropdown.Header>
+            <Link to={'/dashboard?tab=profile'}>
+              <Dropdown.Item>Profile</Dropdown.Item>
+            </Link>
+            <Dropdown.Divider />
+            <Dropdown.Item onClick={handleSignout}>Sign out</Dropdown.Item>
+          </Dropdown>
+        ) : (
+          <Link to='/sign-in'>
+            <Button gradientDuoTone='purpleToBlue' outline>
+              Sign In
+            </Button>
+          </Link>
+        )}
         <Navbar.Toggle/>
       </div>
       <Navbar.Collapse>
