ccab86580c85df3cd003765ba9e15dc306bfc01a
Jerlin Abraham <abrahamjerlin@gmail.com>
2024-06-18 11:39:20 +0530

    add redux persist

--- Client/src/redux/store.js
+++ Client/src/redux/store.js
@@ -1,8 +1,26 @@
-import {configureStore} from '@reduxjs/toolkit'
-import userReducer from './user/userSlice'
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
+import userReducer from './user/userSlice';
+import { persistReducer, persistStore } from 'redux-persist';
+import storage from 'redux-persist/lib/storage'; // Import storage from redux-persist
 
-export const store=configureStore({
-  reducer:{
-    user: userReducer
-  },
-})
+const persistConfig = {
+  key: 'root',
+  storage,
+  version: 1,
+};
+
+const rootReducer = combineReducers({
+  user: userReducer,
+});
+//redux persist is used beacause when we refresh the page our user is erased,with persist user details that we signed in is still reflected in the redux toolkit.
+const persistedReducer = persistReducer(persistConfig, rootReducer); // Pass persistConfig and rootReducer
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
+});
+
+export const persistor = persistStore(store);
