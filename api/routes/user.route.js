7a266964ec98c06132501d30a03cbe38d0a8f9e3
Jerlin Abraham <abrahamjerlin@gmail.com>
2024-06-19 22:18:59 +0530

    add delete user api route

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { test,updateUser } from '../controllers/user.controller.js';
+import { test,updateUser ,deleteUser} from '../controllers/user.controller.js';
 import { verifyToken } from '../utils/verifyUser.js';
 const router = express.Router();
 //everytime writing like below is not a good practice therefore we create a controller:
@@ -10,6 +10,7 @@ router.get('/test',test);
 //first we will verify the token ,if the token is verified ,the user is going to be added to the request in verifyuser.js
 router.put('/update/:userId',verifyToken ,updateUser);
 //the above userid,we can get by req.params(it's basically the id we got from insonmia when we create the user)
+router.delete('/delete/:userId',verifyToken,deleteUser);
 
 //if that id is equal to the id we got when send request in insonmia in our vs code then the user is a valid user
 
