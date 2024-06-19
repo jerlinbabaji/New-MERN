cdbae7fdcc482f3dbc629ca444728efa157a2fb1
Jerlin Abraham <abrahamjerlin@gmail.com>
2024-06-19 19:23:29 +0530

    complete profile page functionality and update functionality

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -60,6 +60,6 @@ export const updateUser = async (req, res, next) => {
         res.status(200).json(rest);
 
     } catch (error) {
-        next(error);
+        next(error+"hhhh");
     }
 };
--- api/utils/verifyUser.js
+++ api/utils/verifyUser.js
@@ -3,12 +3,12 @@ import { errorHandler } from './error.js';
 export const verifyToken = (req, res, next) => {
   const token = req.cookies.access_token;
   if (!token) {
-    return next(errorHandler(401, 'Unauthorized'));
+    return next(errorHandler(401, 'Unauthorizedssssssss'));
   }
   //we need to verify whose token is it that is to verify the token.
   jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
     if (err) {
-      return next(errorHandler(401, 'Unauthorized'));
+      return next(errorHandler(401, 'Unauthorizeddddddd'));
     }
     //first we will verify the token ,if the token is verified ,the user is going to be added to the request.
     //then we will go to the next() whic is updateuser,line 11-->router.put('/update:userId',verifyToken ,updateUser);
